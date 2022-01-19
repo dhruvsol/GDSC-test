96373265c45d783dd2d36bb1c9ce3822722939d9
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-01-19 09:30:24 +0530

    added page 2

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,62 +1,65 @@
-import axios from 'https://cdn.skypack.dev/axios';
+import axios from "https://cdn.skypack.dev/axios";
 
-const first  = await axios({
-    method : "GET",
-    url:"https://jsonplaceholder.typicode.com/photos"
+const first = await axios({
+  method: "GET",
+  url: "https://jsonplaceholder.typicode.com/photos",
+});
 
-})
-// data.length=4;
-// console.log(first.data);
+const second = await axios({
+  method: "GET",
+  url: "https://jsonplaceholder.typicode.com/users",
+});
 
-const second =  await axios({
-    method:"GET",
-    url:"https://jsonplaceholder.typicode.com/users"
-})
-// data2=second.data
 let count = 0;
-const id = document.querySelector('.id')
-const name = document.querySelector('.name')
-const email = document.querySelector('.email')
-const street = document.querySelector('.street')
-const suite = document.querySelector('.suite')
-const city = document.querySelector('.city')
-const zipcode = document.querySelector('.zipcode')
-const Phone_no = document.querySelector('.Phone_no')
-const website = document.querySelector('.website')
-const companyName = document.querySelector('.Company-name')
-const catchPhrase = document.querySelector('.catchPhrase')
-const bs = document.querySelector('bs')
-const forward = document.querySelector('.fa-forward')
-const backward = document.querySelector('.fa-backward')
-const shuffle = document.querySelector('.fa-random')
+const id = document.querySelector(".identifier");
+const name = document.querySelector(".name");
+const email = document.querySelector(".email");
+const street = document.querySelector(".street");
+const suite = document.querySelector(".suite");
+const city = document.querySelector(".city");
+const zipcode = document.querySelector(".zipcode");
+const Phone_no = document.querySelector(".Phone_no");
+const website = document.querySelector(".website");
+const companyName = document.querySelector(".company-name");
+const catchPhrase = document.querySelector(".catch-phrase");
+const bs1 = document.querySelector(".bs");
+const forward = document.querySelector(".forward");
+const backward = document.querySelector(".backward");
+const shuffle = document.querySelector(".shuff");
 
+forward.addEventListener("click", () => {
+  if (count == 9) {
+    count = 0;
+  } else {
+    count += 1;
+  }
+  changeValue();
+});
+backward.addEventListener("click", () => {
+  if (count == 0) {
+    count = 9;
+  } else {
+    count -= 1;
+  }
+  changeValue();
+});
+shuffle.addEventListener("click", () => {
+  count = Math.floor(Math.random() * 10);
 
-
-forward.addEventListener('click',()=>{
-   console.log(count);
-if(count==9){
-    count=0;
-
-}
-    count+=1;
-    changeValue();
-    
-
-
-})
-const changeValue=()=>{
-
-    id.innerHTML=second.data[count].id
-    name.innerHTML=second.data[count].name
-    email.innerHTML=second.data[count].email
-    street.innerHTML=second.data[count].address.street
-    suite.innerHTML=second.data[count].address.suite
-    city.innerHTML=second.data[count].address.city
-    zipcode.innerHTML=second.data[count].address.zipcode
-    Phone_no.innerHTML=second.data[count].phone
-    website.innerHTML=second.data[count].website
-    companyName.innerHTML=second.data[count].company.name
-    catchPhrase.innerHTML=second.data[count].company.catchPhrase
-    bs.innerHTML=second.data[count].company.bs
-}
+  changeValue();
+});
+const changeValue = () => {
+  id.innerHTML = second.data[count].id;
+  name.innerHTML = second.data[count].name;
+  email.innerHTML = second.data[count].email;
+  street.innerHTML = second.data[count].address.street;
+  suite.innerHTML = second.data[count].address.suite;
+  city.innerHTML = second.data[count].address.city;
+  zipcode.innerHTML = second.data[count].address.zipcode;
+  Phone_no.innerHTML = second.data[count].phone;
+  website.innerHTML = second.data[count].website;
+  companyName.innerHTML = second.data[count].company.name;
+  catchPhrase.innerHTML = second.data[count].company.catchPhrase;
+  bs1.innerHTML = second.data[count].company.bs;
+};
 changeValue();
---
+++ assets/scripts/page2.js
@@ -0,0 +1,40 @@
+import axios from "https://cdn.skypack.dev/axios";
+
+const first = await axios({
+  method: "GET",
+  url: "https://jsonplaceholder.typicode.com/photos",
+});
+
+let x =0;
+const id1 = document.querySelector('.id-page2');
+const title  = document.querySelector('.title');
+const src = document.querySelector('.img').getAttribute("src");
+const forward = document.querySelector('.forward');
+const backward = document.querySelector('.backward');
+const shuffle = document.querySelector('.shuffle');
+
+const changeValue = ()=> {
+    id1.innerHTML=first.data[x].id
+    title.innerHTML=first.data[x].title
+    src.innerHTML=first.data[x].thumbnailUrl
+    // console.log(src)
+    // console.log(first.data[x].thumbnailUrl)
+}
+forward.addEventListener('click',()=>{
+    x+=1;
+    changeValue();
+})
+backward.addEventListener('click',()=>{
+    if(x==0){
+        x=5000;
+    }else {
+        x-=1
+    }
+    changeValue();
+})
+shuffle.addEventListener('click',()=>{
+   x= Math.floor(Math.random()*5000)
+   changeValue();
+})
+
+changeValue();
